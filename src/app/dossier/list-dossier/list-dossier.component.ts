3d37d94142b94e8517d43ff2e1522bf72031d78d
Mohamed <medmostafaa@gmail.com>
2019-11-08 23:16:43 +0100

    test commit branch moustapha

--- src/app/dossier/list-dossier/list-dossier.component.ts
+++ src/app/dossier/list-dossier/list-dossier.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DossierService} from "../../sevices/dossier/dossier.service";
 
 @Component({
   selector: 'app-list-dossier',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListDossierComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dossierService: DossierService) { }
 
   ngOnInit() {
+  }
+  getDossiers(){
+    this.dossierService.getAll().then(res=>{
+      console.log(res);
+    })
   }
 
 }
--- src/app/sevices/dossier/dossier.service.ts
+++ src/app/sevices/dossier/dossier.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Dossier} from "../../classes/dossier";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DossierService {
-
-  constructor(private http:HttpClient) { }
+   base_url =environment.url + 'dossiers';
+  constructor(private http:HttpClient) {
+  }
+  getAll() : Promise<Dossier[]>{
+    return this.http.get<Dossier[]>(this.base_url).toPromise();
+  }
 }
